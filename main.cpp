1ffe406577d83ac0968ce28b5aff64b52643386b
mrkamis <kamilsamiec@gmail.com>
2019-04-27 14:18:05 +0200

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+    cout << "\n";
+    system("PAUSE");
+    return 1;
+}
